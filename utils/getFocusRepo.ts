dc03c223cfcfb0a444d36ca20306a40fb7d70a6d
zhangfisher <wxzhang@126.com>
2024-08-07 17:27:28 +0800

    update

--- utils/getFocusRepo.ts
+++ utils/getFocusRepo.ts
@@ -3,11 +3,11 @@ import { getUrlHash } from "./getUrlHash";
 
 export function getFocusRepo(repos:Repo[],defaultRepo:string='voerka-i18n'){
     let repoName = getUrlHash()
-    console.log("focus repo name:",repoName)    
     if(repoName.trim()=='') repoName = defaultRepo
     let repo = repos.find(repo => repo.name === repoName)
     if(!repo){
         repo = repos.find(repo => repo.name === defaultRepo)
     }
+    console.log("focus repo :",repo)    
     return repo
 }
